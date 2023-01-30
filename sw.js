9a26a137006cb22373bd8e0353602ec227e6eaea
Dună <alexdunarentu@gmail.com>
2023-01-30 10:10:21 +0200

    Modified sw

--- sw.js
+++ sw.js
@@ -12,9 +12,18 @@ function loadScript(src, callback) {
   document.querySelector('head').appendChild(script);
 }
 
+loadScript('https://www.googletagmanager.com/gtag/js?id=G-MNDGKLC9EV', function () {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+     dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-MNDGKLC9EV');
+});
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(
+    navigator.serviceWorker.register('sw.js').then(
       function (registration) {
         console.log('Service worker registered: ', registration.scope);
       },
@@ -25,11 +34,3 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-loadScript('https://www.googletagmanager.com/gtag/js?id=G-MNDGKLC9EV', function () {
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', G - MNDGKLC9EV);
-});
