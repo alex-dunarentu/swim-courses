308d4ab37c6d9da5e32ddcc59360d49a11047bf4
Dună <alexdunarentu@gmail.com>
2023-01-30 19:04:27 +0200

    Offline analytics

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+// use workbox to configure the `gtag.js` script to be cached and used offline
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+
+workbox.googleAnalytics.initialize({
+  parameterOverrides: {
+    cd1: 'offline',
+  },
+  hitFilter: (params) => {
+    const queueTimeInSeconds = Math.round(Date.now() / 1000) - params.queueTime;
+    params.cd1 = queueTimeInSeconds;
+    return true;
+  },
+});
+
+// register a route for the `gtag.js` script using Workbox
+workbox.routing.registerRoute(
+  new RegExp('https://www.googletagmanager.com/gtag/js'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'gtag-js',
+  })
+);
+
 // cache resources
 const addResourcesToCache = async (resources) => {
   const cache = await caches.open('v1');
